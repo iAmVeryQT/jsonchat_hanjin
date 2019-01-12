1c5619a1d97f8e6a0b38cf325bea3d56c2e039d3
iAmVeryQT <46300504+iAmVeryQT@users.noreply.github.com>
2019-01-12 22:41:03 +0900

    가이드수정

--- jsonchat/mainwindow.cpp
+++ jsonchat/mainwindow.cpp
@@ -49,8 +49,9 @@ void MainWindow::on_chatWindow_returnPressed()
     msg += "'room': '#1',";
     msg += "'text': '" + chatMsg + "'";
     msg += "} #json end";
-    char* temp = msg.toUtf8().data();
-    jsonServer.write(temp, strlen(temp));
+    jsonServer.write(msg.toUtf8().data()); // 가이드> toUtf8().data()는 toUtf8()호출시
+    // 인스턴스가 즉시 만들어지고 사라지므로 data()의 리턴값을 바로 전달해야 합니다.
+
     // clear UI
     ui->chatWindow->setText("");
 }
@@ -63,6 +64,7 @@ void MainWindow::recvEchoMsg()
     if(isExistPackets) {
         char* buffer = new char[packetSize + 1];
         jsonServer.read(buffer, packetSize);
+        buffer[packetSize] = '\0'; // 가이드> 문자열의 끝을 넣어줘야 합니다.
         QMessageBox(QMessageBox::Information, TITLE, buffer).exec();
     } else {
         QMessageBox(QMessageBox::Information, TITLE, "msg recv fail.").exec();
